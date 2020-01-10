0ccbb8d44d061d5e1e30a7414e2670367bc00f37
Ivan Stefanyshyn <ivanstefanushun@gmail.com>
2020-01-10 18:12:07 +0200

    feat: added class collection

--- src/utils/collection.js
+++ src/utils/collection.js
@@ -1,5 +1,25 @@
 import _ from 'lodash';
 
+class Collection{
+    elements = [];
+    /**
+     * 
+     * @param {string} name - name of collection 
+     */
+    constructor(name){
+        this.name = name;    
+        this._loadFromStorage();
+    }
+
+    
+
+    _saveToStorage = () => {
+        localStorage.setItem(`__collection__${this.name}`, JSON.stringify(this.elements));
+    }
+    _loadFromStorage(){
+        this.elements =  JSON.parse(localStorage.getItem(`__collection__${this.name}`));
+    }
+}
 
 
 module.exports = Collection;
